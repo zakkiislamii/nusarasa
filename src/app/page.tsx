61241b5d4a0bab41f16869f85216bc5e87f6eef7
zakkiislamii <mzislami05@gmail.com>
2024-09-25 22:29:55 +0700

    feat: Setup Pre-commit Husky

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
 
 export default function Home() {
+  console.log(1);
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+      <div className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
@@ -49,7 +50,7 @@ export default function Home() {
           />
           Go to nextjs.org →
         </a>
-      </footer>
+      </div>
     </div>
   );
 }
