d69b3451e3d66f2ddc935d98c1c7ec785b1bceb7
zakkiislamii <mzislami05@gmail.com>
2024-09-25 21:38:01 +0700

    tes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-     
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
